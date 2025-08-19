327d398aeed6b815c4d8a1b1ff330cdd9999cae3
shaiknaseervali <shaiknaseervali@gmail.com>
2025-08-19 12:39:01 +0530

    fixed path to live

--- src/vendordashboard/data/ApiPath.js
+++ src/vendordashboard/data/ApiPath.js
@@ -1,2 +1,2 @@
-export const API_URL="http://localhost:4000";
-// const API_URL="https://backend-suby-g0r4.onrender.com/"
+// export const API_URL="http://localhost:4000";
+export const API_URL="https://backend-suby-g0r4.onrender.com/"
